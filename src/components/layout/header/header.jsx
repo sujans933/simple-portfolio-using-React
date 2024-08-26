3ddddf45fa6aa7c07aa2f7678f8b0533d390e992
Sujan Shrestha <sujans933@gmail.com>
2024-08-26 18:57:19 +0545

    header continue

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -4,23 +4,23 @@ import MaxWidthWrapper from "../../section/MaxWidthWrapper";
 function Header() {
   const lists = [
     { title: "home", link: "/#" },
-    { title: "Portfolio", link: "/#" },
-    { title: "About me", link: "/#" },
+    { title: "portfolio", link: "/#" },
+    { title: "about me", link: "/#" },
     { title: "testimonials", link: "/#" },
   ];
   return (
     <MaxWidthWrapper>
-      <div className="flex justify-between items-center py-[44px] ">
+      <div className="flex justify-between items-center py-[25px] ">
         <img src="Logo.png" alt="logo" width="186px" height="40px" />
-        <div className="flex gap-[42px] text-[21px] font-normal capitalize ">
+        <div className="flex gap-[42px] capitalize font-normal text-[21px] ">
           {lists.map((list, index) => (
-            <a className="hover:text-primary " href={list.link}>
+            <a className="hover:text-primary" href={list.link} key={index}>
               {list.title}
             </a>
           ))}
         </div>
-        <button className="border-[1.5px] border-primary text-primary rounded-[5px] py-[18px] px-[42px] ">
-          Contact me
+        <button className="bg-primary text-white hover:bg-white hover:border-[1.5px] hover:border-primary hover:text-primary rounded-[5px] px-[42px] py-[18px] ">
+          Contact Me
         </button>
       </div>
     </MaxWidthWrapper>
--- src/components/section/MaxWidthWrapper.jsx
+++ src/components/section/MaxWidthWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react";
+import React from "react";
 
 function MaxWidthWrapper({ children }) {
   return <div className="px-[86px]">{children}</div>;
